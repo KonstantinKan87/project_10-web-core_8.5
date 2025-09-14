18e489dde11125a186526fec60bc9399fce6301e
KonstantinKan87 <kostya.kan.87@gmail.com>
2025-09-14 15:34:12 +0900

    second commit

--- script.js
+++ script.js
@@ -1,21 +1,40 @@
 const swiper = new Swiper(".swiper", {
-  // Optional parameters
-  direction: "vertical",
-  loop: true,
+  slidesPerView: 2,
+  spaceBetween: 10,
+  direction: "horizontal",
 
-  // If we need pagination
   pagination: {
     el: ".swiper-pagination",
+    clickable: true,
   },
+  breakpoints: {
+    768: {
+      slidesPerView: 6,
+      grid: { rows: 2 },
+    },
+    1120: {
+      slidesPerView: 8,
+      grid: { rows: 2 },
+    },
+  },
+});
 
-  // Navigation arrows
-  navigation: {
-    nextEl: ".swiper-button-next",
-    prevEl: ".swiper-button-prev",
-  },
+const btn = document.getElementById("toggleBtn");
+const icon = document.getElementById("toggleIcon"); 
+const hiddenSlides = document.querySelectorAll(".swiper-slide.hidden");
 
-  // And if we need scrollbar
-  scrollbar: {
-    el: ".swiper-scrollbar",
-  },
+btn.addEventListener("click", (e) => {
+  e.preventDefault();
+
+  hiddenSlides.forEach(slide => slide.classList.toggle("hidden"));
+
+  if (btn.textContent === "Показать все") {
+    btn.textContent = "Скрыть";
+    icon.src = "./assets/images/expand.png"; 
+  } else {
+    btn.textContent = "Показать все";
+    icon.src = "./assets/images/icon.png";  
+  }
+
+  swiper.update();
 });
